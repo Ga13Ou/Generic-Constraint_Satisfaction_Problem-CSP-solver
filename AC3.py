57c8d2026bcd8a9337a2bd21c4905b6a0844a985
Gastli Oussama <gast.oussama@gmail.com>
2018-12-05 19:08:51 +0100

    finishing AC1

--- AC3.py
+++ AC3.py
@@ -6,6 +6,7 @@ class AC3:
 
     def __init__(self):
         self.constraints_array = []
+        self.constants_constraints_array = []  # this is for the constraints between a variable and a constant example: var2=5
         self.constraints_graph = []
         self.variables = []
         self.assigned = []
@@ -13,6 +14,9 @@ class AC3:
 
     def addConstraint(self, var1, var2, verifier):
         self.constraints_array.append([var1, var2, verifier])
+
+    def addConstantConstraint(self, varIndex, constant, verifier):
+        self.constants_constraints_array.append([varIndex, constant, verifier])
 
     def buildConstraintGraph(self):
         # you have to set Variables number and add all constraints before using this method
@@ -28,16 +32,34 @@ class AC3:
         self.constraints_graph = [[] for i in range(N)]
 
     def diffVerifier(self, i, j):
+        if self.assigned[i] * self.assigned[j] == 0:
+            return True
         return self.variables[i] != self.variables[j]
 
     def eqVerifier(self, i, j):
+        if self.assigned[i] * self.assigned[j] == 0:
+            return True
         return self.variables[i] == self.variables[j]
 
     def sameDiagonalVerifier(self, i, j):
+        if self.assigned[i] * self.assigned[j] == 0:
+            return True
         return abs(i - j) == abs(self.variables[i] - self.variables[j])
 
     def notSameDiagonalVerifier(self, i, j):
+        if self.assigned[i] * self.assigned[j] == 0:
+            return True
         return not self.sameDiagonalVerifier(i, j)
+
+    def constEqVerifier(self, i, a):
+        if self.assigned[i] == 0:
+            return True
+        return self.variables[i] == a
+
+    def constDiffVerifier(self, i, a):
+        if self.assigned[i] == 0:
+            return True
+        return self.variables[i] != a
 
     def setDomains(self, array):
         self.domains = array
@@ -69,19 +91,78 @@ class AC3:
                     self.addConstraint(int(tab[1]), int(tab[2]), self.eqVerifier)
                 elif tab[0] == "notSameDiagonal":
                     self.addConstraint(int(tab[1]), int(tab[2]), self.notSameDiagonalVerifier)
+                elif tab[0] == "constEq":
+                    self.addConstantConstraint(tab[1], tab[2], self.constEqVerifier)
+                elif tab[0] == "constDiff":
+                    self.addConstantConstraint(tab[1], tab[2], self.constDiffVerifier)
 
             else:
                 if tab[0] == "allDifferent":
                     for i in range(len(self.variables)):
-                        for j in range(i+1, len(self.variables)):
+                        for j in range(i + 1, len(self.variables)):
                             self.addConstraint(i, j, self.diffVerifier)
 
+                if tab[0] == "allNotSameDiag":
+                    for i in range(len(self.variables)):
+                        for j in range(i + 1, len(self.variables)):
+                            self.addConstraint(i, j, self.notSameDiagonalVerifier)
 
-test=AC3()
+    def checkConstraint(self, constraint):
+        return constraint[2](constraint[0], constraint[1])
+
+    def checkAllConstantConstraints(self):
+        for constraint in self.constants_constraints_array:
+            if not self.checkAllConstraints(constraint):
+                return False
+        return True
+
+    def checkAllVarConstraints(self):
+        for contraint in self.constraints_array:
+            if not self.checkConstraint(contraint):
+                return False
+        return True
+
+    def checkAllConstraints(self):
+        return self.checkAllConstantConstraints() and self.checkAllVarConstraints()
+
+    def reviser(self, i, j):
+        deleted = False
+        index1 = 0
+        while index1 < len(self.domains[i]):
+            go = False
+            val1 = self.domains[i][index1]
+            for index2, val2 in enumerate(self.domains[j]):
+                self.variables[i] = val1
+                self.variables[j] = val2
+                self.assigned[i] = self.assigned[j] = 1
+                if self.checkAllConstraints():
+                    go = True
+                # cleaning:
+                self.assigned[i] = self.assigned[j] = 0
+                if go:
+                    break
+            if not go:
+                self.domains[i].pop(index1)
+                index1 -= 1
+                deleted = True
+            index1 += 1
+        return deleted
+
+    def AC1(self):
+        go = True
+        while go:
+            go = False
+            for i in range(len(self.variables)):
+                for j in range(len(self.variables)):
+                    if i != j:
+                        self.reviser(i, j)
+
+
+test = AC3()
 test.parseProblemFromFile()
 test.buildConstraintGraph()
-
-pprint(test.constraints_array)
-print(test.constraints_graph)
+test.AC1()
+# pprint(test.constraints_array)
+# print(test.constraints_graph)
 print(test.domains)
 print(test.variables)
