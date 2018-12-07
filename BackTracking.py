a410b313275b382f0886e75c0175225f6698f7b7
Gastli Oussama <gast.oussama@gmail.com>
2018-12-07 11:57:28 +0100

    adding AC3 to BT successfully

--- BackTracking.py
+++ BackTracking.py
@@ -1,28 +1,40 @@
 from CSP import CSP
+from AC3 import AC3
 from copy import deepcopy
 
 
-class BackTracking(CSP):
+class BackTracking(AC3):
 
     def BT(self, index: int):
-        heurIndex=self.convert_index(index)
         if index == len(self.variables):
             return True
+        heurIndex = self.convert_index(index)
         self.updateHeuristics()
         for k in range(len(self.domains[self.heur_array[heurIndex]])):
             self.variables[self.heur_array[heurIndex]] = self.domains[self.heur_array[heurIndex]][k]
-            self.assigned[self.heur_array[heurIndex]] = 1
-            heur_array_copy=deepcopy(self.heur_array) #Save
+            self.assigned[self.heur_array[heurIndex]] = 1 #Save
+            heur_array_copy=deepcopy(self.heur_array)
+            currentVarDomainCopy = deepcopy(self.domains[self.heur_array[heurIndex]])
+            self.domains[self.heur_array[heurIndex]] = [self.variables[self.heur_array[heurIndex]]]
+            domainsCopy=deepcopy(self.domains)
             if (self.checkAllConstraints()):
+                if self.AC3_is_activated:
+                    # print("allo")
+                    self.arcConsistency3(heurIndex)
+                    # print("am here")
                 if (self.BT(index + 1)):
                     return True
-            self.assigned[self.heur_array[heurIndex]] = 0
-            self.heur_array=heur_array_copy #Restore
+            self.assigned[self.heur_array[heurIndex]] = 0 #Restore
+            self.heur_array=heur_array_copy
+            self.domains=domainsCopy
+            self.domains[self.heur_array[heurIndex]] = currentVarDomainCopy
         return False
 
     def solve(self):
-        print("tessst")
-        self.BT(0)
+        if self.AC3_is_activated:
+            self.initArcConsistency3()
+            print("init Done")
+        print(self.BT(0))
 
 
 # test
@@ -30,7 +42,7 @@ a = BackTracking()
 # a.setProblemFileName("testExample/mapColoringProblem.txt")
 a.setProblemFileName("testExample/sudokuProblem.txt")
 a.parseProblemFromFile()
-
+a.activatedAC3()
 a.activateHeuristicsMrvDh()
 a.solve()
 print(a.variables)
