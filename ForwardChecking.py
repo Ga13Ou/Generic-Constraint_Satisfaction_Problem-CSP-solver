274ff60e6d19df023d1e77b1131602637d73d397
Gastli Oussama <gast.oussama@gmail.com>
2018-12-07 11:00:24 +0100

    adding AC3 to FC succesfully

--- ForwardChecking.py
+++ ForwardChecking.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
 from CSP import CSP
+from AC3 import AC3
 from pprint import pprint
 
 
-class ForwardChecking(CSP):
+class ForwardChecking(AC3):
 
     def FC(self, varIndex: int, varArray: list, varDomain):
         for i in range(len(self.variables)):
@@ -23,28 +24,39 @@ class ForwardChecking(CSP):
         return True
 
     def forwardSolver(self, currentIndex, varArray, varDomain):
-        heurIndex=self.convert_index(currentIndex)
-        self.updateHeuristics()
         if currentIndex == len(self.variables):
             self.variables = varArray
             self.domains = varDomain
             return True
+        heurIndex=self.convert_index(currentIndex)
+        if self.AC3_is_activated:
+            # print("allo")
+            self.arcConsistency3(heurIndex)
+            # print("am here")
+        self.updateHeuristics()
+
         for value in varDomain[self.heur_array[heurIndex]]:
             varArray[self.heur_array[heurIndex]] = value
             self.assigned[self.heur_array[heurIndex]] = 1
             varArrayCopy = deepcopy(varArray)  # Save
             varDomainCopy = deepcopy(varDomain)
             heur_array_copy=deepcopy(self.heur_array)
+            currentVarDomainCopy=deepcopy(self.domains[self.heur_array[heurIndex]])
+            self.domains[self.heur_array[heurIndex]]=[self.variables[self.heur_array[heurIndex]]]
             if self.FC(currentIndex, varArray, varDomain):
                 if self.forwardSolver(currentIndex + 1, varArray, varDomain):
                     return True
             self.variables = varArray = varArrayCopy  # restore
             self.domains = varDomain = varDomainCopy
             self.heur_array=heur_array_copy
+            self.domains[self.heur_array[heurIndex]]=currentVarDomainCopy
             self.assigned[self.heur_array[heurIndex]] = 0
         return False
 
     def solve(self):
+        if self.AC3_is_activated:
+            self.initArcConsistency3()
+            print("init Done")
         print(self.forwardSolver(0, self.variables, self.domains))
 
 
@@ -54,6 +66,7 @@ a = ForwardChecking()
 a.setProblemFileName("testExample/sudokuProblem.txt")
 a.parseProblemFromFile()
 a.activateHeuristicsMrvDh()
+# a.activatedAC3()
 a.solve()
 print(a.variables)
 expected="397286541412539768856471329284195637639748215571362894728913456163854972945627183"
