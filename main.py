09decae787f841edac60df8ad879067e6de59a52
Gastli Oussama <gast.oussama@gmail.com>
2018-12-07 22:27:41 +0100

    finishing main file

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def main():
     heur=input("Activer l'heuristique MRV et DH? [O/N]: ")
     ac3=input("Activer l'utilisation de l'algorithme AC3? [O/N]: ")
 
+
+    AC3_print="Non"
+    heur_print="Non"
+    algorithm_print="BackTracking"
     if algo.lower()=="fc":
         solver=ForwardChecking()
+        algorithm_print = "ForwardChecking"
+
     else:
         solver=BackTracking()
     if file_name !="":
@@ -18,16 +24,25 @@ def main():
     solver.parseProblemFromFile()
     if heur.lower()=="o":
         solver.activateHeuristicsMrvDh()
+        heur_print="Oui"
     if ac3.lower()=="o":
         solver.activateAC3()
+        AC3_print="Oui"
     start_time=time.time()
     result=solver.solve()
     finish_time=time.time()
+
+
+    print("\n")
+    print("Rapport:")
+    print("\talgorithm: {}".format(algorithm_print))
+    print("\tAC3 active: {}".format(AC3_print))
+    print("\tHeuristique MRV et DH active: {}".format(heur_print))
+    print("\ttemps d'execution: {} secondes".format(finish_time-start_time))
+    print("\tnombre d'iterations: {}".format(solver.get_number_of_iterations()))
     if result:
-        print(solver.variables)
+        print("\tsolution: "+str(solver.variables))
     else:
-        print("le probleme n'a pas de solution")
-    print("temps d'execution: {} secondes".format(finish_time-start_time))
-    print("nombre d'iterations: {}".format(solver.get_number_of_iterations()))
+        print("\tle probleme n'a pas de solution")
 
 main()
